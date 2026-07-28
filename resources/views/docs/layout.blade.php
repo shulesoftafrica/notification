b560f6c89d993fb5e59853f8081df9b256d91955
Ephraim Swilla <swillae1@gmail..com>
2026-07-28 21:47:20 +0300

    comment dead url

--- resources/views/docs/layout.blade.php
+++ resources/views/docs/layout.blade.php
@@ -269,7 +269,7 @@
                     <i class="fas fa-webhook mr-2"></i> Webhook Tester
                 </a>
 
-                <div class="mt-6 mb-2 px-4 text-xs font-semibold text-gray-500 uppercase">
+                {{-- <div class="mt-6 mb-2 px-4 text-xs font-semibold text-gray-500 uppercase">
                     Guides
                 </div>
 
@@ -291,7 +291,7 @@
 
                 <a href="{{ route('docs.guides', 'rate-limits') }}" class="nav-item block px-4 py-2.5 rounded text-sm">
                     <i class="fas fa-tachometer-alt mr-2"></i> Rate Limits
-                </a>
+                </a> --}}
 
                 <div class="mt-6 mb-2 px-4 text-xs font-semibold text-gray-500 uppercase">
                     Resources
